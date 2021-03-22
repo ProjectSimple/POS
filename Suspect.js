0f5d6dd9929f130b00e3fb1022e30f09888d2bc2
Reid Luna <reidluna@icloud.com>
2021-03-22 11:59:09 -0700

    Start V3

---
+++ Suspect.js
@@ -0,0 +1,3 @@
+exports("Suspect", () => {
+  console.log("suspect");
+});
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,14 @@
 fx_version 'bodacious'
 game 'gta5'
 
-author 'Reid Luna <reidluna@icloud.com>'
-version '2.0.0'
+author 'You'
+version '1.0.0'
 
-client_scripts 'client.js'
+client_scripts {
+  'Keys.js',
+  'Marker.js',
+  'Officer.js',
+  'Suspect.js',
+  'client.js'
+}
 server_script 'server.js'
